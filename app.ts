e4cdda67a385575be5531755db8e80d24b2be60b
paula.saenzda <paula.saenzda@gmail.com>
2026-01-08 12:33:53 +0000

    cors change

--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors( {
-    origin: ['https://fake-store-api-front.vercel.app/', 'http://localhost:3000']
+    origin: ['https://fake-store-api-front.vercel.app', 'http://localhost:3000']
   }));
 app.use(express.json());
 
